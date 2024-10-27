2b471ff972e4cc933b15727515455e6d939ae6c4
Budhsheela Maurya <bssmaurya123@gmail.com>
2024-10-27 14:44:06 +0530

    compose-app: added API_KEY and internet permission issue

--- app/src/main/java/com/misri/weather_compose_android_app/MainActivity.kt
+++ app/src/main/java/com/misri/weather_compose_android_app/MainActivity.kt
@@ -23,20 +23,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContent {
-//            WeathercomposeandroidappTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        setContent {
+            WeathercomposeandroidappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 //                    Greeting(
 //                        name = "Android",
 //                        modifier = Modifier.padding(innerPadding)
 //                    )
-//                }
-//            }
-//        }
-        setContent {
-            val viewModel: WeatherViewModel = hiltViewModel()
-            WeatherScreen(viewModel)
+                    val viewModel: WeatherViewModel = hiltViewModel()
+                    WeatherScreen(viewModel, modifier = Modifier.padding(innerPadding))
+                }
+            }
         }
+//        setContent {
+//            val viewModel: WeatherViewModel = hiltViewModel()
+//            WeatherScreen(viewModel)
+//        }
     }
 }
 
--- app/src/main/java/com/misri/weather_compose_android_app/presentation/ui/WeatherScreen.kt
+++ app/src/main/java/com/misri/weather_compose_android_app/presentation/ui/WeatherScreen.kt
@@ -6,16 +6,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import com.misri.weather_compose_android_app.presentation.viewmodels.WeatherViewModel
 
 @Composable
-fun WeatherScreen(viewModel: WeatherViewModel) {
+fun WeatherScreen(viewModel: WeatherViewModel, modifier: Modifier = Modifier) {
     val weatherState by viewModel.weatherState.collectAsState()
 
     Column {
-        Text("Weather Info")
-        Button(onClick = { viewModel.fetchWeather("London") }) {
-            Text("Get Weather")
+        Text("Weather Info", modifier = modifier)
+        Button(onClick = { viewModel.fetchWeather("London") }
+        ) {
+            Text("Get Weather", modifier = modifier)
         }
         weatherState?.let {
             WeatherCard(weather = it)
